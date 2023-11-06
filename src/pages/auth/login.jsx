e400d249d5aefbdff9831141b673ba1d5dd3c151
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-06 16:51:41 +0700

    feat: structured folder

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className=" flex flex-row justify-center items-center h-screen w-screen bg-gray-100">
+      <h1 className="text-black">hallo</h1>
+    </div>
+  );
+}
