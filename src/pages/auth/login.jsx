3eed9bb076d2e25c08d0bca2c38cdf4f1dcfd625
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-06 21:14:41 +0700

    feat: finish create right auth component

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+
 export default function Login() {
   return (
-    <div className=" flex flex-row justify-center items-center h-screen w-screen bg-gray-100">
-      <h1 className="text-black">hallo</h1>
+    <div className=" flex flex-row justify-center items-center h-screen w-screen bg-neutral-100">
+      <div className="h-[calc(100vh-20px)] w-1/2 relative bg-red-300 mt-20 mb-20 rounded-lg ml-[10px]"></div>
+      <div className="flex flex-col justify-center items-center h-[calc(100vh-20px)] w-1/2 relative bg-dark-blue rounded-lg mr-[10px] ml-[10px] ">
+        <Image
+          src="/logo.png"
+          alt="Picture of the author"
+          width={100}
+          height={100}
+          className="absolute top-3 right-5" // just an example
+        />
+
+        <Image
+          src="/auth_image.png"
+          alt="Picture of the author"
+          width={600}
+          height={600}
+          className="mb-20" // just an example
+        />
+        <h1 className="self-start pl-[120px] font-semibold text-[40px]">
+          Sign in to TICKET.IN
+        </h1>
+        <p className="self-start pl-[120px] mb-[100px] font-light text-[20px]">
+          Welcome back! Please enter your credentials to access your Ticket.in
+          account.
+        </p>
+      </div>
     </div>
   );
 }
