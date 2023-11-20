f1f0eb0ff7ca24b78ea7635cc1e4140552508c69
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-20 19:10:47 +0700

    feat: finish create detail event page

--- src/pages/event/event_detail.jsx
+++ src/pages/event/event_detail.jsx
@@ -5,6 +5,31 @@ import HeaderComp from "@/components/header_comp";
 import { BsCalendarWeek } from "react-icons/bs";
 
 export default function EventDetail() {
+  const ticketPrice = 68000;
+
+  const [buyNow, setBuyNow] = useState(false); // [false, function
+  const [count, setCount] = useState(1);
+  const [totalTicketPrice, setTotalTicketPrice] = useState(ticketPrice);
+  const IDR = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
+
+  const handleBuyNow = () => {
+    setBuyNow(true);
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+      setTotalTicketPrice(ticketPrice * (count - 1));
+    }
+  };
+  const increment = () => {
+    setCount(count + 1);
+    setTotalTicketPrice(ticketPrice * (count + 1));
+  };
+
   return (
     <div className="flex flex-col justify-center items-center bg-neutral-100">
       <HeaderComp />
@@ -16,21 +41,21 @@ export default function EventDetail() {
             className="w-5/6 px-7 py-10"
           />
         </div>
-        <div className="flex flex-col items-start w-full px-[200px]">
+        <div className="flex flex-col items-start w-full px-[40px] sm:px-[100px] lg:px-[200px]">
           <p className="self-start text-blue-text font-extrabold text-[30px] ">
             Akurat Festival
           </p>
           <p className="text-[30px] text-blue-text font-normal">by Akurat</p>
           <div className="flex flex-row ">
-            <BsCalendarWeek className="text-[30px] text-blue-text font-normal mt-2" />
+            <BsCalendarWeek className="text-[30px] text-blue-text font-normal mt-2 min-w-[27px] w-[27px] lg:w-[50px]" />
             <div className="flex flex-col">
-              <p className="text-[30px] text-blue-text font-normal ml-2">
+              <p className="text-[23px] lg:text-[30px] text-blue-text font-normal ml-2">
                 12 Desember 2021, 13.00 - 22.00 WIB
               </p>
-              <p className="text-[30px] text-blue-text font-bold ml-2">
+              <p className="text-[23px] lg:text-[30px] text-blue-text font-bold ml-2">
                 Secapa AD
               </p>
-              <p className="text-[30px] text-blue-text font-normal ml-2">
+              <p className="text-[23px] lg:text-[30px] text-blue-text font-normal ml-2">
                 Jl. Hegarmanah No. 152, Bandung Kota, Bandung Kota, Jawa Barat,
                 Indonesia
               </p>
@@ -52,12 +77,51 @@ export default function EventDetail() {
             also allowed and able to speak up and take part in the arts and
             creativity industry.
           </p>
-          <div className="flex justify-between rounded-xl px-7 py-2 mt-10 items-center bg-blue-text w-full font-bold shadow-[0_25px_50px_-12px_rgba(56,57,157,0.3)]">
-            <p className="text-xl">IDR 68k</p>
-            <button className="text-white  w-[120px] bg-[#F5167E] py-3 font-medium  rounded-[100px] hover:opacity-90 ">
-              Buy Now
-            </button>
-          </div>
+
+          {buyNow ? (
+            <div className="self-center  w-1/2 flex flex-col min-w-[400px] bg-blue-text px-10 py-5 mt-10 rounded-xl shadow-[0_25px_50px_-12px_rgba(56,57,157,0.3)]">
+              <h1 className="text-[#F5167E] text-[30px] font-bold">
+                Your Order
+              </h1>
+              <p className="text-[20px]">
+                Accurate Festival Presale 1 09 Dec '23, 13.00-22.00 WIB{" "}
+                {IDR.format(ticketPrice)}
+              </p>
+              <div className="flex justify-end items-center w-full mb-5 mt-10">
+                <button
+                  onClick={decrement}
+                  className="bg-[#F5167E] w-10 h-10 rounded-full text-[23px]"
+                >
+                  -
+                </button>
+                <p className="text-[20px] mx-5">{count}</p>
+                <button
+                  onClick={increment}
+                  className="bg-[#F5167E] w-10 h-10 rounded-full text-[23px]"
+                >
+                  +
+                </button>
+              </div>
+              <div className="w-full h-[1px] bg-white mb-6 opacity-70"></div>
+              <div className="flex justify-between">
+                <p className="text-[20px]">Total</p>
+                <p className="text-[20px]">{IDR.format(totalTicketPrice)}</p>
+              </div>
+              <button className="bg-[#F5167E] w-1/2 self-center py-2 mt-5 rounded-full text-[17px] lg:text-[23px]">
+                BUY TICKET
+              </button>
+            </div>
+          ) : (
+            <div className="flex justify-between rounded-xl px-7 py-2 mt-10 items-center bg-blue-text w-full font-bold shadow-[0_25px_50px_-12px_rgba(56,57,157,0.3)]">
+              <p className="text-xl">IDR 68k</p>
+              <button
+                onClick={handleBuyNow}
+                className="text-white  w-[120px] bg-[#F5167E] py-3 font-medium  rounded-[100px] hover:opacity-90 "
+              >
+                Buy Now
+              </button>
+            </div>
+          )}
           <h1 className="text-[30px] text-blue-text font-normal ml-2 mt-20">
             Important
           </h1>
