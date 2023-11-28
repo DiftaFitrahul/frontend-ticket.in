9aa7337cff4e2f51ec0f04e6fe423d563b4ee6e8
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-28 09:57:14 +0700

    feat: finish sidebar and modify headercomp

---
+++ src/components/SidebarComp.jsx
@@ -0,0 +1,98 @@
+import { useState, useContext } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/../public/logo.png";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { logout } from "@/redux/authSlice";
+import Cookies from "js-cookie";
+import { BsSearch } from "react-icons/bs";
+import { FiMenu } from "react-icons/fi";
+import { SidebarContext } from "@/components/HeaderComp";
+
+import { toast } from "react-toastify";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { showSidebar, setShowSidebar } = useContext(SidebarContext);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  console.log(isLoggedIn);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    Cookies.remove("Auth");
+    toast.success("Logout Berhasil!"),
+      {
+        zIndex: 9999,
+      };
+  };
+
+  return (
+    <div
+      className={`bg-[#242565] text-white   h-screen   pb-5 pt-4 z-[10] fixed right-0 top-0 transition-all ${
+        showSidebar ? "w-64" : "w-0 "
+      }`}
+    >
+      <div className="flex justify-between items-center px-3 mb-4 w-full ">
+        <Image
+          src={Logo}
+          alt="Picture of the author"
+          unoptimized
+          className="w-[130px]  mr-5"
+        />
+        <button
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="text-white hover:text-[#242565] hover:bg-white font-bold w-[40px] h-[40px] mr-2 border border-white rounded-xl"
+        >
+          X
+        </button>
+      </div>
+      <div className="flex flex-col w-full h-[150px] px-3">
+        <Link
+          href=""
+          className="text-white hover:text-[#242565] pl-3 pt-3   h-full px-full w-full hover:bg-white rounded-xl font-semibold"
+        >
+          Ticket
+        </Link>
+
+        <Link
+          href="/about"
+          className="text-white hover:text-[#242565] pl-3 pt-3 h-full px-full w-full font-semibold hover:bg-white rounded-xl font-semibold"
+        >
+          About
+        </Link>
+
+        <Link
+          href=""
+          className="text-white hover:text-[#242565] h-full pl-3 pt-3 px-full w-full font-semibold hover:bg-white rounded-xl font-semibold"
+        >
+          Contact
+        </Link>
+      </div>
+      <div className="h-4/6 xl:h-3/4  flex flex-col">
+        <div className="mt-auto mb-10 ml-4">
+          {isLoggedIn ? (
+            <Link
+              href="/"
+              onClick={handleLogout}
+              className="border text-white border-white py-2 px-9 rounded-full mr-5"
+            >
+              Logout
+            </Link>
+          ) : (
+            <Link
+              href="/auth/login"
+              className="border font-semibold text-white hover:text-[#242565] hover:bg-white border-white py-2 px-9 rounded-full mr-5"
+            >
+              Login
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,7 @@ import { useContext } from "react";
 import { LoadingContext } from "@/context/LoadingContext";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Sidebar from "@/components/SidebarComp";
 
 export default function Home() {
   const [eventsArray, setEventsArray] = useState([]);
@@ -17,7 +18,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    localStorage.removeItem('selectedEventData');
+    localStorage.removeItem("selectedEventData");
 
     fetchEventData();
   }, []);
@@ -40,86 +41,91 @@ export default function Home() {
   const handleCardEventClick = (index) => {
     setIsLoading(true);
     const selectedEventData = eventsArray[index];
-    localStorage.setItem('selectedEventData', JSON.stringify(selectedEventData));
+    localStorage.setItem(
+      "selectedEventData",
+      JSON.stringify(selectedEventData)
+    );
     console.log(selectedEventData);
-    router.push('/event/detail');
+    router.push("/event/detail");
     setIsLoading(false);
   };
 
   return (
     <>
-    <Head>
-			<title>Home | ticket.in</title>
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		</Head>
+      <Head>
+        <title>Home | ticket.in</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-    <main>
-      <div className="flex flex-col justify-center items-center bg-neutral-100">
-        <div className="relative flex justify-center items-center w-full min-h-screen ">
-          <img
-            src="/home/bg_image.png"
-            alt="Picture of the author"
-            className="absolute w-full h-full"
-          />
-          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-[#922455] to-[#C6B13F] opacity-[0.9]"></div>
-          <HeaderComp />
-          <div className="text-black relative z-[2] flex flex-col mt-10 xl:mt-0 xl:flex-row justify-center  items-center ml-10">
+      <main>
+        <div className="flex flex-col justify-center items-center bg-neutral-100">
+          <div className="relative flex justify-center items-center w-full min-h-screen ">
             <img
-              src="/home/poster.png"
-              alt="coba"
-              className="min-w-[500px] w-3/4 max-w-[800px]"
+              src="/home/bg_image.png"
+              alt="Picture of the author"
+              className="absolute w-full h-full"
             />
-            <div className="w-[400px] sm:w-[550px]  xl:mr-10 mt-5 xl:mt-0">
-              <p className="text-[18px] font-bold text-white"> Movie Party</p>
-              <h1 className="text-[50px] font-bold text-white leading-[50px] mb-3">
-                Petualangan Sherina 2
-              </h1>
-              <p className="text-[18px] font-light text-white">
-                SHERINA (Sherina Munaf) dan SADAM (Derby Romero), dua teman
-                kecil yang lama terpisah, bertemu kembali di Kalimantan untuk
-                pelepasliaran orang utan.
-              </p>
-              <div className="flex mt-3">
-                <button className="bg-[#DF1875] h-[50px] w-[160px] text-white font-medium rounded-full text-[18px] mr-5">
-                  Get Ticket
-                </button>
-                <button className="border border-white h-[50px] w-[160px] text-white font-medium rounded-full text-[18px]">
-                  Learn More
-                </button>
-              </div>
-            </div>
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-[#922455] to-[#C6B13F] opacity-[0.9]"></div>
+            <HeaderComp />
 
-            <IoIosArrowForward className="text-[#3D37F1] text-[60px] right-[20px] text-white hidden xl:block" />
+            <div className="text-black relative z-[2] flex flex-col mt-10 xl:mt-0 xl:flex-row justify-center  items-center ml-10">
+              <img
+                src="/home/poster.png"
+                alt="coba"
+                className="min-w-[500px] w-3/4 max-w-[800px]"
+              />
+              <div className="w-[400px] sm:w-[550px]  xl:mr-10 mt-5 xl:mt-0">
+                <p className="text-[18px] font-bold text-white"> Movie Party</p>
+                <h1 className="text-[50px] font-bold text-white leading-[50px] mb-3">
+                  Petualangan Sherina 2
+                </h1>
+                <p className="text-[18px] font-light text-white">
+                  SHERINA (Sherina Munaf) dan SADAM (Derby Romero), dua teman
+                  kecil yang lama terpisah, bertemu kembali di Kalimantan untuk
+                  pelepasliaran orang utan.
+                </p>
+                <div className="flex mt-3">
+                  <button className="bg-[#DF1875] h-[50px] w-[160px] text-white font-medium rounded-full text-[18px] mr-5">
+                    Get Ticket
+                  </button>
+                  <button className="border border-white h-[50px] w-[160px] text-white font-medium rounded-full text-[18px]">
+                    Learn More
+                  </button>
+                </div>
+              </div>
+
+              <IoIosArrowForward className="text-[#3D37F1] text-[60px] right-[20px] text-white hidden xl:block" />
+            </div>
+            <div className="text-black relative block xl:hidden">
+              <IoIosArrowForward className="text-[#3D37F1] text-[60px] right-[20px] text-white " />
+            </div>
           </div>
-          <div className="text-black relative block xl:hidden">
-            <IoIosArrowForward className="text-[#3D37F1] text-[60px] right-[20px] text-white " />
+          <div className="flex flex-col min-[1350px]:flex-row  justify-between w-4/6 mt-[100px] mb-[100px] justify-center items-center">
+            <div className="text-[#242565] text-[40px]">Upcoming Events</div>
+            <div className="flex flex-wrap mt-5 xl:mt-0  justify-center items-center h-[150px]">
+              <FilterButton title="Weekdays" />
+              <FilterButton title="Event Type" className="my-5 sm:my-0" />
+              <FilterButton title="Any Category" />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col min-[1350px]:flex-row  justify-between w-4/6 mt-[100px] mb-[100px] justify-center items-center">
-          <div className="text-[#242565] text-[40px]">Upcoming Events</div>
-          <div className="flex flex-wrap mt-5 xl:mt-0  justify-center items-center h-[150px]">
-            <FilterButton title="Weekdays" />
-            <FilterButton title="Event Type" className="my-5 sm:my-0" />
-            <FilterButton title="Any Category" />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 min-[800px]:grid-cols-2 xl:grid-cols-3 gap-7">
+          <div className="grid grid-cols-1 min-[800px]:grid-cols-2 xl:grid-cols-3 gap-7">
             {eventsArray.map((event, index) => (
               <button onClick={() => handleCardEventClick(index)}>
-                <CardEvent key={index}
-                  price= {event.eventPrice}
-                  quota= {event.eventQuota}
-                  title= {event.eventName}
-                  subtitle= {event.eventDescription}
+                <CardEvent
+                  key={index}
+                  price={event.eventPrice}
+                  quota={event.eventQuota}
+                  title={event.eventName}
+                  subtitle={event.eventDescription}
                   imagePath="/home/event_image.png"
                 />
               </button>
-              ))}
+            ))}
+          </div>
+          <MakeEvent />
+          <FooterComp />
         </div>
-        <MakeEvent />
-        <FooterComp />
-      </div>
-    </main>
+      </main>
     </>
   );
 }
