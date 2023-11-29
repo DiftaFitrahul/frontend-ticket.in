f5c09a995863a8657864f5941c78489492661b98
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-29 08:44:04 +0700

    fix: responsive card in home

--- src/components/home/CardEvent.jsx
+++ src/components/home/CardEvent.jsx
@@ -1,20 +1,30 @@
-export default function CardEvent({ title, subtitle, imagePath, quota, price }) {
+export default function CardEvent({
+  title,
+  subtitle,
+  imagePath,
+  quota,
+  price,
+}) {
   const IDR = new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
   });
 
   return (
-    <div className="flex flex-col w-[410px] h-[400px]  bg-white rounded-3xl">
+    <div className="flex flex-col w-[200px] min-[300px]:w-[300px] min-[450px]:w-[410px] h-[400px]  bg-white rounded-3xl">
       <img
         src={imagePath}
         alt="coba"
         className="w-full h-[250px] object-fill"
       />
-      <div className="flex items-start justify-start  mt-4 mx-6">
-        <div className="flex flex-col items-center mr-5 mt-4">
-          <p className="text-[#3D37F1] text-center font-bold text-[15px] mt-1">{IDR.format(price)}</p>
-          <p className="text-black font-bold text-[20px]">{quota} Pcs</p>
+      <div className="flex items-start justify-start  mt-4 mx-3 min-[300px]:mx-6">
+        <div className="flex flex-col items-center mr-2 min-[300px]:mr-5 mt-4">
+          <p className="text-[#3D37F1] text-center font-bold text-[10px] min-[300px]:text-[15px] mt-1">
+            {IDR.format(price)}
+          </p>
+          <p className="text-black font-bold text-[10px] min-[300px]:text-[20px]">
+            {quota} Pcs
+          </p>
         </div>
         <div className="flex flex-col ">
           <h1 className="text-black font-bold text-[21px]">{title}</h1>
