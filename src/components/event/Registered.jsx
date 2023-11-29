3a71e34a03a7831273069dd7bdb7b84f32194fcc
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-29 09:55:49 +0700

    fix: responsive registered event page

--- src/components/event/Registered.jsx
+++ src/components/event/Registered.jsx
@@ -11,28 +11,30 @@ export default function EventsRegisteredComp({
   return (
     <div className="w-3/4 bg-white rounded-3xl shadow-md mt-[45px] lg:mt-[30px]">
       <div className="flex flex-col lg:flex-row items-center  w-full  py-5 ">
-        <div className="flex w-[300px] sm:w-[400px]  ml-5 justify-center">
+        <div className="flex w-[140px] min-[280px]:w-[200px] min-[350px]:w-[250px] min-[480px]:w-[340px] sm:w-[400px]  ml-5 justify-center">
           <img
             src={imagePath}
             alt="coba"
-            className="w-[300px] sm:w-full h-[230px] min-w-[300px] sm:min-w-[400px] self-center "
+            className="w-full h-[230px]  self-center "
           />
         </div>
-        <div className="flex  flex-col  justify-start items-start w-full items-start justify-start ml-20 mt-5 lg:mt-0 lg:ml-4 xl:ml-10 mr-5">
-          <h1 className="text-black font-bold text-[26px]">{title}</h1>
+        <div className="flex  flex-col  justify-start items-start w-full items-start justify-start ml-20  mt-5 lg:mt-0 lg:ml-4 xl:ml-10 mr-5">
+          <h1 className="text-black font-bold text-[16px] min-[280px]:text-[20px] sm:text-[26px]">
+            {title}
+          </h1>
           <div className="flex flex-row ">
-            <BsCalendarWeek className="text-[30px] text-blue-text font-normal mt-2 min-w-[27px] w-[27px] lg:w-[50px]" />
+            <BsCalendarWeek className="text-[30px] text-blue-text font-normal lg:mt-2 min-w-[20px] sm:min-w-[27px] w-[20px] sm:w-[27px] lg:w-[50px]" />
             <div className="flex flex-col">
-              <p className="text-[23px] lg:text-[30px] text-blue-text font-normal ml-2">
+              <p className="text-[18px] sm:text-[23px] lg:text-[30px] text-blue-text font-normal ml-2">
                 {eventDate}
               </p>
             </div>
           </div>
           <p className="text-[#6A6A6A] mr-20 lg:mr-0">{description}</p>
-          <p className="text-black font-semibold text-[15px] mt-5">
+          <p className="text-black font-semibold text-[11px] sm:text-[15px] mt-5">
             Total ticket: {ticketTotal} Pcs
           </p>
-          <p className="text-[#3D37F1] text-center font-bold text-[15px] ">
+          <p className="text-[#3D37F1] text-center font-bold text-[11px] sm:text-[15px] ">
             Total Harga: Rp {priceTotal}
           </p>
         </div>
--- src/pages/event/registered.jsx
+++ src/pages/event/registered.jsx
@@ -9,7 +9,7 @@ export default function EventsRegistered() {
       imagePath: "/home/event_image.png",
       title: "Denny Caknan",
       description:
-        "Event ini menyajikan konser deny caknan yang sangat bagus dan akdhkah dkahsdhask jdhakduwkdhka sjdhakuwd hakdhuakjsd aushduoashduosaudasoud memuakaias asdasdaljhdiajsdljadjl",
+        "Event ini menyajikan konser deny caknan yang sangat bagus dan event ini akan menampilkan guest star terbaru bersama dengan hal-hal yang sangat menarik lainnya tunggu apalagi ayo daftar",
       eventDate: "12 Agustus 2021",
       ticketTotal: "10",
       priceTotal: "20000",
@@ -18,7 +18,7 @@ export default function EventsRegistered() {
       imagePath: "/home/event_image.png",
       title: "Denny Caknan",
       description:
-        "Event ini menyajikan konser deny caknan yang sangat bagus dan akdhkah dkahsdhask jdhakduwkdhka sjdhakuwd hakdhuakjsd aushduoashduosaudasoud memuakaias asdasdaljhdiajsdljadjl",
+        "Event ini menyajikan konser deny caknan yang sangat bagus dan event ini akan menampilkan guest star terbaru bersama dengan hal-hal yang sangat menarik lainnya tunggu apalagi ayo daftar",
       eventDate: "12 Agustus 2021",
       ticketTotal: "10",
       priceTotal: "20000",
@@ -27,7 +27,7 @@ export default function EventsRegistered() {
       imagePath: "/home/event_image.png",
       title: "Denny Caknan",
       description:
-        "Event ini menyajikan konser deny caknan yang sangat bagus dan akdhkah dkahsdhask jdhakduwkdhka sjdhakuwd hakdhuakjsd aushduoashduosaudasoud memuakaias asdasdaljhdiajsdljadjl",
+        "Event ini menyajikan konser deny caknan yang sangat bagus dan event ini akan menampilkan guest star terbaru bersama dengan hal-hal yang sangat menarik lainnya tunggu apalagi ayo daftar",
       eventDate: "12 Agustus 2021",
       ticketTotal: "10",
       priceTotal: "20000",
@@ -36,7 +36,7 @@ export default function EventsRegistered() {
       imagePath: "/home/event_image.png",
       title: "Denny Caknan",
       description:
-        "Event ini menyajikan konser deny caknan yang sangat bagus dan akdhkah dkahsdhask jdhakduwkdhka sjdhakuwd hakdhuakjsd aushduoashduosaudasoud memuakaias asdasdaljhdiajsdljadjl",
+        "Event ini menyajikan konser deny caknan yang sangat bagus dan event ini akan menampilkan guest star terbaru bersama dengan hal-hal yang sangat menarik lainnya tunggu apalagi ayo daftar",
       eventDate: "12 Agustus 2021",
       ticketTotal: "10",
       priceTotal: "20000",
@@ -45,7 +45,7 @@ export default function EventsRegistered() {
       imagePath: "/home/event_image.png",
       title: "Denny Caknan",
       description:
-        "Event ini menyajikan konser deny caknan yang sangat bagus dan akdhkah dkahsdhask jdhakduwkdhka sjdhakuwd hakdhuakjsd aushduoashduosaudasoud memuakaias asdasdaljhdiajsdljadjl",
+        "Event ini menyajikan konser deny caknan yang sangat bagus dan event ini akan menampilkan guest star terbaru bersama dengan hal-hal yang sangat menarik lainnya tunggu apalagi ayo daftar",
       eventDate: "12 Agustus 2021",
       ticketTotal: "10",
       priceTotal: "20000",
@@ -56,7 +56,9 @@ export default function EventsRegistered() {
     <div className="flex flex-col justify-center items-center bg-neutral-100">
       <HeaderComp />
       <div className="mt-[160px]">
-        <h1 className="text-black font-bold text-[36px]">Event Registered</h1>
+        <h1 className="text-black text-center font-bold text-[36px]">
+          Event Registered
+        </h1>
       </div>
 
       {listDataEventRegistered.map((dataEventRegistered, index) => {
