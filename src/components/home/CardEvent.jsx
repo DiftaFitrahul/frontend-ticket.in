e7539a6d7a133e1dd2c725d7b5bd0850cfa50c29
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-29 08:49:28 +0700

    fix: finish fixing card event

--- src/components/home/CardEvent.jsx
+++ src/components/home/CardEvent.jsx
@@ -11,22 +11,22 @@ export default function CardEvent({
   });
 
   return (
-    <div className="flex flex-col w-[200px] min-[300px]:w-[300px] min-[450px]:w-[410px] h-[400px]  bg-white rounded-3xl">
+    <div className="flex flex-col w-[200px] min-[330px]:w-[300px] min-[450px]:w-[410px] h-[400px] shadow-xl  bg-white rounded-3xl">
       <img
         src={imagePath}
         alt="coba"
         className="w-full h-[250px] object-fill"
       />
-      <div className="flex items-start justify-start  mt-4 mx-3 min-[300px]:mx-6">
-        <div className="flex flex-col items-center mr-2 min-[300px]:mr-5 mt-4">
-          <p className="text-[#3D37F1] text-center font-bold text-[10px] min-[300px]:text-[15px] mt-1">
+      <div className="flex items-start justify-start  mt-4 mx-3 min-[330px]:mx-6">
+        <div className="flex flex-col items-center mr-2 min-[330px]:mr-5 mt-4">
+          <p className="text-[#3D37F1] text-center font-bold text-[10px] min-[330px]:text-[15px] mt-1">
             {IDR.format(price)}
           </p>
-          <p className="text-black font-bold text-[10px] min-[300px]:text-[20px]">
+          <p className="text-black font-bold text-[10px] min-[330px]:text-[20px]">
             {quota} Pcs
           </p>
         </div>
-        <div className="flex flex-col items-start bg-red-500">
+        <div className="flex flex-col items-start">
           <h1 className="text-black font-bold text-[21px]">{title}</h1>
           <p className="text-[#6A6A6A] text-start">{subtitle}</p>
         </div>
