349b07c9caa0362751e48021c94a16742ccfb415
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-11-29 11:46:58 +0700

    fix: responsive confirm

--- src/pages/payment/identity/confirm.jsx
+++ src/pages/payment/identity/confirm.jsx
@@ -96,31 +96,31 @@ export default function ConfirmIdentity() {
     <div className="flex flex-col justify-center items-center bg-neutral-100">
       <HeaderComp />
       <div className="flex flex-col items-start p-10 lg:p-20 bg-white mt-[200px] w-[calc(70vw)]  mb-[50px] rounded-xl shadow-[0_25px_50px_-12px_rgba(56,57,157,0.3)]">
-        <h1 className="text-[#242565] font-extrabold text-[40px]">
+        <h1 className="text-[#242565] font-extrabold text-[20px]  min-[350px]:text-[30px] sm:text-[40px]">
           Attende Information
         </h1>
-        <p className="text-[#242565]  text-[20px]">
+        <p className="text-[#242565] text-[17px] sm:text-[20px]">
           Make sure all the information provided is correct. You cannot change
           it later
         </p>
 
         <form onSubmit={handleSubmit} className="flex flex-col w-full mt-5 ">
-          <p className="text-black font-medium text-[20px] mt-3 sm:text-[30px]">
+          <p className="text-black font-medium text-[15px] sm:text-[20px] mt-3 sm:text-[30px]">
             {userData.name}
           </p>
-          <p className="text-black font-medium text-[20px] mt-3 sm:text-[30px]">
+          <p className="text-black font-medium text-[15px] sm:text-[20px] mt-3 sm:text-[30px]">
             {userData.email}
           </p>
-          <p className="text-black font-medium text-[20px] mt-3 sm:text-[30px]">
+          <p className="text-black font-medium text-[15px] sm:text-[20px] mt-3 sm:text-[30px]">
             {userData.phoneNumber}
           </p>
 
-          <label className="flex items-start w-3/4 mt-20 bg-red">
+          <label className="flex items-start w-4/4 mt-20 bg-red">
             <input
               type="checkbox"
               checked={isChecked}
               onChange={handleCheckboxChange}
-              className="form-checkbox min-h-[20px] min-w-[20px] text-blue-500"
+              className="form-checkbox min-h-[20px] min-w-[20px] mt-2 text-blue-500"
             />
             <span className="ml-8 text-black ">
               I agree to the Goers Terms and Conditions and the Goers Privacy
@@ -131,7 +131,7 @@ export default function ConfirmIdentity() {
 
           <button
             type="submit"
-            className="mt-[40px] self-center bg-[#F5167E] py-4 w-[300px] rounded-full shadow-xl font-medium"
+            className="mt-[40px] self-center bg-[#F5167E] py-4 w-[140px] min-[350px]:w-[250px]  rounded-full shadow-xl font-medium"
           >
             Continue
           </button>
