89a731154fd1d772b04f511a5eedb23371527a2c
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-12-04 16:26:36 +0700

    fix: change img tag into image tag in card event compt

--- src/components/home/CardEvent.jsx
+++ src/components/home/CardEvent.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function CardEvent({
   title,
   subtitle,
@@ -11,11 +12,14 @@ export default function CardEvent({
   });
 
   return (
-    <div className="flex flex-col w-[240px] min-[370px]:w-[350px] md:w-[380px]  xl:w-[410px] h-[400px] shadow-xl  bg-white rounded-3xl">
-      <img
+    <div className="flex flex-col w-[240px] min-[370px]:w-[350px] md:w-[380px]  xl:w-[410px] h-[400px] shadow-xl  bg-white rounded-3xl ">
+      <Image
         src={imagePath}
         alt="coba"
-        className="w-full h-[250px] object-fill"
+        width={1000} // Set a large number to allow for a responsive width
+        height={250}
+        objectFit="cover"
+        className="self-center  rounded-xl"
       />
       <div className="flex items-start justify-start  mt-4 mx-3 min-[330px]:mx-6">
         <div className="flex flex-col items-center mr-2 min-[330px]:mr-5 mt-4">
